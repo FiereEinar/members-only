f49aa4cfb83a5491e3346a716058561432780abb
FiereEinar <nickxylanmelloria@gmail.com>
2024-06-04 16:08:42 +0800

    added sign up form

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
-exports.signUp = asyncHandler(async (req, res) => {
-  res.send('TODO: Implement sign up page');
+exports.sign_up_get = asyncHandler(async (req, res) => {
+  res.render('forms/sign_up', {
+    user: undefined
+  });
 });
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const authController = require('../controllers/authController');
 
-router.get('/sign_up', authController.signUp);
+router.get('/sign_up', authController.sign_up_get);
 
 module.exports = router;
